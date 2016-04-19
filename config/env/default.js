f8f96cc90ad6324d9957b03bdb30a62855eac5c2
Aruljothi Kuppusamy <jak.jothi@gmail.com>
2016-04-19 21:42:00 +0530

    #TEL-48 version bump 1.1.0

--- config/env/default.js
+++ config/env/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     title: 'Telemedco Watson Service',
-    version: '1.0.0',
+    version: '1.1.0',
     port: process.env.VCAP_APP_PORT || 3000,
     host: process.env.VCAP_APP_HOST || 'localhost'
 };
